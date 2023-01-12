15099e76c279ff84300a39d9dbdc0a5993e3c0fb
kbulloc10 <kahlea.bullock@my.strayer.edu>
2023-01-12 09:14:08 -0800

    TEMPERATURE CONVERSION LAB

--- Temperature_Conversion_WK2_LB2.py
+++ Temperature_Conversion_WK2_LB2.py
@@ -1,1 +1,25 @@
+#Kahlea Bullock CIS261 TEMPERATURE CONVERSION LAB
+
+
+def to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def to_fahrenheit(celsius):
+    fahrenheit = celsius * 9/5 + 32
+    return fahrenheit
+
+
+def main():
+    for temp in range(0, 212, 40):
+        print(temp, "Fahrenheit =", round(to_celsius(temp), 2),
+              "Celsius")
+    
+    for temp in range(0, 100, 20):
+        print(temp, "Celsius =", round(to_fahrenheit(temp), 2),
+              "Fahrenheit")
+
+
+if __name__ == "__main__":
+    main()
 
